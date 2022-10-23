906fb0e681c01effe807d2fbaab066ed30458faa
dhanireddygovardhanreddy <dhanireddygovardhanreddy@gmail.com>
2022-10-23 20:55:49 +0530

    Spacewar:welcome PEX!
    Signed-off-by: dhanireddygovardhanreddy <dhanireddygovardhanreddy@gmail.com>

--- aosp_spacewar.mk
+++ aosp_spacewar.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PEX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from Spacewar device
 $(call inherit-product, device/nothing/Spacewar/device.mk)
 
 PRODUCT_DEVICE := Spacewar
-PRODUCT_NAME := lineage_Spacewar
+PRODUCT_NAME := aosp_Spacewar
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A063
 PRODUCT_MANUFACTURER := nothing
